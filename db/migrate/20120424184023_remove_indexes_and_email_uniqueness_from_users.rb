770c147ce4623ffb967c8c93ff38b92c8896295e
rcavezza <rcavezza@gmail.com>
2012-04-24 23:39:40 -0400

    fix migration error

--- db/migrate/20120424184023_remove_indexes_and_email_uniqueness_from_users.rb
+++ db/migrate/20120424184023_remove_indexes_and_email_uniqueness_from_users.rb
@@ -1,14 +1,14 @@
 class RemoveIndexesAndEmailUniquenessFromUsers < ActiveRecord::Migration
   def up
-  	remove_column :users, :email, :string
-  	remove_column :users, :encrypted_password, :string 
+  	remove_column :users, :email
+  	remove_column :users, :encrypted_password 
     add_column :users, :email, 							:string
     add_column :users, :encrypted_password, :string
   end
 
   def down
-  	remove_column :users, :email, :string
-  	remove_column :users, :encrypted_password, :string 
+  	remove_column :users, :email
+  	remove_column :users, :encrypted_password
     add_column :users, :email, 							:string, :null => false, :default => ""
     add_column :users, :encrypted_password, :string, :null => false, :default => ""
   end
