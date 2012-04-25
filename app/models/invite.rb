6493d189fd64fb79f3035919c4786b65d01dd5f5
rcavezza <rcavezza@gmail.com>
2012-04-25 19:42:10 -0400

    add linkedin_id to attr accessible in invite model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,4 +1,4 @@
 class Invite < ActiveRecord::Base
-	attr_accessible :first_name, :last_name, :body, :title, :subject
+	attr_accessible :first_name, :last_name, :body, :title, :subject, :linkedin_id
 	belongs_to :user 
 end
