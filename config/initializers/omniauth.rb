96d75f474274cae68ff1c7afde4a46caa931fd95
rcavezza <rcavezza@gmail.com>
2012-04-30 01:27:45 -0400

    corects error - send_message to linkedin now converts data to json

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -12,6 +12,6 @@ LinkedIn::Client.class_eval do
 # options = {:recipients => {:values => [:person => {:_path => "/people/~" }, :person =>   {:_path => "/people/USER_ID"} ]}, :subject => "Something",:body => "To read" }
   def send_message(options)
     path = "/people/~/mailbox"
-    post(path, options, "Content-Type" => "application/json")
+    post(path, options.to_json, "Content-Type" => "application/json")
   end
 end 
