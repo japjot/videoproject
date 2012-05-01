fd0420e194a37463e2117202df9a18be3d1ab48e
rcavezza <rcavezza@gmail.com>
2012-04-30 21:17:37 -0400

    fixed styles to show 4 per column and fixed width of videos

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,7 +1,12 @@
 module VideosHelper
 
 	def show_video(yt_client, video)
-		return raw yt_client.video_by(video.youtube_id).embed_html_with_width(350)
+		@embed_video = '<object width="260" height="164">'
+		@embed_video = @embed_video + '<param value="http://www.youtube.com/v/'+video.youtube_id+'&feature=youtube_gdata_player" name="movie">'
+		@embed_video = @embed_video + '<param value="transparent" name="wode">'
+		@embed_video = @embed_video + '<embed width="260" height="164" wmode="transparent" type="application/x-shockwave-flash" src="http://www.youtube.com/v/'+video.youtube_id+'&feature=youtube_gdata_player">'
+		@embed_video = @embed_video + '</object>'
+		return @embed_video.html_safe 
 	end 
 
 	def show_large_video(yt_client, video)
