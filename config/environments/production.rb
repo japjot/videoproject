c0af8ed03619bbc63fb977ac01dd93489ec12617
rcavezza <rcavezza@gmail.com>
2012-05-01 21:37:24 -0400

    added email host in production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Videoapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.default_url_options = { :host => 'radiant-warrior-8123.herokuapp.com/' }
+
 
   # Code is not reloaded between requests
   config.cache_classes = true
