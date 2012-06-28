3d8099ae9769d5fb6814db30ea2d42e922c0f232
Bob Cavezza <rcavezza@gmail.com>
2012-06-28 16:40:51 -0400

    add ilike for postgres where queries

--- app/models/video.rb
+++ app/models/video.rb
@@ -17,7 +17,7 @@ class Video < ActiveRecord::Base
 
 	def self.search(search)
 		if search
-	    	find(:all, :conditions => ['name LIKE ? or summary LIKE ?', "%#{search}%", "%#{search}%" ])
+	    	find(:all, :conditions => ['name ILIKE ? or summary ILIKE ?', "%#{search}%", "%#{search}%" ])
 		else
 			find(:all)
 		end
