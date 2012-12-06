9716b2f8af25d96aadde70cf04bf63d339df5845
Bob Cavezza <rcavezza@gmail.com>
2012-12-05 19:00:54 -0500

    can now save videos in admin backend w/o error

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,3 +1,24 @@
 ActiveAdmin.register Video do
+
+	# before_save do |video|
+ #  	puts video.to_yaml
+ #  	@url = video.url
+ #    if(@url.downcase.include? 'youtube.com')
+ #      ##create ways to catch if user is using 
+ #      puts query_string = URI.parse(video.url).query
+ #      puts parameters = Hash[URI.decode_www_form(query_string)]
+ #      puts parameters['v'] # => aNdMiIAlK0g
+ #      video_id = parameters['v']
+ #    elsif (@url.downcase.include? 'y2u.be')
+ #      puts video_id = @url.match(/\.be\/(.+)$/)[1]
+ #      @url = 'http://www.youtube.com/watch?v='+video_id
+ #      video.url = @url 
+ #    elsif (@url.downcase.include? 'youtu.be')
+ #      puts video_id = @url.match(/\.be\/(.+)$/)[1]
+ #      @url = 'http://www.youtube.com/watch?v='+video_id
+ #      video.url = @url
+ #    end
+ #    video.youtube_id = video_id
+ #  end
   
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,18 +2,39 @@ class Video < ActiveRecord::Base
 	acts_as_voteable
 	acts_as_commentable
 	
-	attr_accessible :name, :summary, :source, :url, :location
+	attr_accessible :name, :summary, :source, :url, :location, :user_id, :duration
 
-	validates :user_id, presence: true
-	validates :duration, presence: true 
-	validates_inclusion_of :duration, :in => 0..60, :message => "Video must be shorter than 60 seconds"
+	# validates :user_id, presence: true
+	# validates :duration, presence: true 
+	# validates_inclusion_of :duration, :in => 0..60, :message => "Video must be shorter than 60 seconds"
 	validates_format_of :url, :with => /^(http:\/\/)?(m\.)?(?:www\.)?youtube.com\/watch\?(?=.*v=\w+)(?:(?:\S(?!http))+)?$/,
 														:message => 'this is not a valid youtube url'
-	validates :youtube_id, presence: true 
+	# validates :youtube_id, presence: true 
 
 
 
-	belongs_to :user 
+	# belongs_to :user 
+
+  before_save do |video|
+  	puts video.to_yaml
+  	@url = video.url
+    if(@url.downcase.include? 'youtube.com')
+      ##create ways to catch if user is using 
+      puts query_string = URI.parse(video.url).query
+      puts parameters = Hash[URI.decode_www_form(query_string)]
+      puts parameters['v'] # => aNdMiIAlK0g
+      video_id = parameters['v']
+    elsif (@url.downcase.include? 'y2u.be')
+      puts video_id = @url.match(/\.be\/(.+)$/)[1]
+      @url = 'http://www.youtube.com/watch?v='+video_id
+      video.url = @url 
+    elsif (@url.downcase.include? 'youtu.be')
+      puts video_id = @url.match(/\.be\/(.+)$/)[1]
+      @url = 'http://www.youtube.com/watch?v='+video_id
+      video.url = @url
+    end
+    video.youtube_id = video_id
+  end
 
 	def home_page_featured_description
 		return self.summary
